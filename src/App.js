9be9e134c433ac869ee5f4c4b5ef7a622aea2982
Susanto <nerdyspook007@gmail.com>
2021-11-26 23:52:54 +0530

    routes added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
 
 export default function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+
+          <Route path="/projects">
+            <Projects />
+          </Route>
+
+          <Route path="/contact">
+            <Contact />
+          </Route>
+
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 }
