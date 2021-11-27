df27124583d0cf20f1f7998d4febe284c019a1a9
Susanto <nerdyspook007@gmail.com>
2021-11-27 08:40:15 +0530

    styled components added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,52 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+const NavbarStyles = styled.div`
+  position: fixed;
+  z-index: 100;
+  top: 0;
+  left: 0;
+  width: 100%;
+  padding: 1rem;
+  background-color: var(--dark-bg);
+
+  ul {
+    max-width: 1200px;
+    margin: 0 auto;
+    width: 90%;
+    text-align: center;
+
+    li {
+      display: inline-block;
+      border-radius: 0.5rem;
+      transition: 0.3s ease background-color;
+      &:hover {
+        background-color: var(--deep-dark);
+      }
+    }
+    a {
+      display: inline-block;
+      font-family: 'RobotoMono Regular';
+      padding: 1rem 2rem;
+      font-size: 2rem;
+      color: var(--gray-1);
+      outline: none;
+    }
+    .active {
+      color: var(--white);
+    }
+  }
+`;
 
 export default function Navbar() {
   return (
-    <div>
+    <NavbarStyles>
       <ul>
         <li>
-          <NavLink to="/">Home</NavLink>
+          <NavLink exact to="/">
+            Home
+          </NavLink>
         </li>
         <li>
           <NavLink to="/about">About</NavLink>
@@ -18,6 +58,6 @@ export default function Navbar() {
           <NavLink to="/contact">Contact</NavLink>
         </li>
       </ul>
-    </div>
+    </NavbarStyles>
   );
 }
