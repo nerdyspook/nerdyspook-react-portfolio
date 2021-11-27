b78765e137050dd397edd13d6936f9e4582d921a
Susanto <nerdyspook007@gmail.com>
2021-11-27 10:52:39 +0530

    responsiveness added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+
+import { MdClose, MdMenu } from 'react-icons/md';
 import styled from 'styled-components';
 
 const NavbarStyles = styled.div`
@@ -35,6 +37,57 @@ const NavbarStyles = styled.div`
     }
     .active {
       color: var(--white);
+      // font-size: 2.2rem;
+    }
+  }
+
+  .mobile-menu-icon {
+    position: absolute;
+    right: 1rem;
+    top: 1rem;
+    width: 4rem;
+
+    cursor: pointer;
+    outline: none;
+
+    display: none;
+  }
+
+  .closeNavIcon {
+    display: none;
+  }
+
+  @media screen and (max-width: 768px) {
+    .mobile-menu-icon {
+      display: block;
+    }
+
+    .navItems {
+      transition: 0.3s ease transform;
+      background-color: var(--deep-dark);
+      padding: 2rem;
+      max-width: 300px;
+      width: 90%;
+      border-radius: 1rem;
+
+      position: absolute;
+      right: 1rem;
+      top: var(--top);
+
+      .closeNavIcon {
+        display: block;
+        width: 3rem;
+        margin: 0 0 0 auto;
+
+        cursor: pointer;
+        * {
+          pointer-events: none;
+        }
+      }
+      li {
+        display: block;
+        margin-bottom: 1rem;
+      }
     }
   }
 `;
@@ -42,7 +95,13 @@ const NavbarStyles = styled.div`
 export default function Navbar() {
   return (
     <NavbarStyles>
-      <ul>
+      <div className="mobile-menu-icon">
+        <MdMenu />
+      </div>
+      <ul className="navItems">
+        <div className="closeNavIcon">
+          <MdClose />
+        </div>
         <li>
           <NavLink exact to="/">
             Home
--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -13,6 +13,8 @@ const GlobalStyles = createGlobalStyle`
     --gray-2: #363636;
     --white : white;
     --black: black;
+
+    --top: 1rem;
   }
   html{
     font-size: 10px;
