c8c0e09c9d670ac13e9211da7ed42a6ae2458a9f
Susanto <nerdyspook007@gmail.com>
2021-11-28 13:41:28 +0530

    services section added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+
 import AboutSection from '../components/AboutSection';
 import HeroSection from '../components/HeroSection';
+import ServicesSection from '../components/ServicesSection';
 
 export default function home() {
   return (
     <div>
       <HeroSection />
       <AboutSection />
+      <ServicesSection />
     </div>
   );
 }
