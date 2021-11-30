84332829872fbc69001d5e002eb9f61bbafb5a8f
Susanto <nerdyspook007@gmail.com>
2021-11-30 10:31:46 +0530

    testimonial section added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import AboutSection from '../components/AboutSection';
 import HeroSection from '../components/HeroSection';
 import ProjectsSection from '../components/ProjectsSection';
 import ServicesSection from '../components/ServicesSection';
+import TestimonialsSection from '../components/TestimonialsSection';
 
 export default function home() {
   return (
@@ -12,6 +13,7 @@ export default function home() {
       <AboutSection />
       <ServicesSection />
       <ProjectsSection />
+      <TestimonialsSection />
     </div>
   );
 }
